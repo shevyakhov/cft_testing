2ecea15204c0160121f7ae5bf2e68a91ae7d5b61
shevyakhov <shevyakhov89@gmail.com>
2022-09-13 20:22:49 +0700

    greeting function

--- app/src/main/java/com/example/cft_testing/greeting/GreetFragment.kt
+++ app/src/main/java/com/example/cft_testing/greeting/GreetFragment.kt
@@ -1,18 +1,17 @@
 package com.example.cft_testing.greeting
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.cft_testing.R
 import com.example.cft_testing.databinding.FragmentGreetBinding
 
-class GreetFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GreetFragment()
-    }
+class GreetFragment : Fragment() {
 
     private val viewModel by viewModels<GreetViewModel>()
     private var _binding: FragmentGreetBinding? = null
@@ -28,6 +27,14 @@ class GreetFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.greetBtn.setOnClickListener {
+            val alert: AlertDialog.Builder = AlertDialog.Builder(context)
+            alert.setTitle(viewModel.greetCurrentUser())
+            alert.setPositiveButton(getText(R.string.hello)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            alert.show()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/cft_testing/greeting/GreetViewModel.kt
+++ app/src/main/java/com/example/cft_testing/greeting/GreetViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.cft_testing.greeting
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.cft_testing.R
 
-class GreetViewModel : ViewModel() {
+class GreetViewModel(val app: Application) : AndroidViewModel(app) {
+    private fun getCurrentUser(): String {
+        return "sup"
+    }
+
+    fun greetCurrentUser(): String {
+        return "${app.getString(R.string.hello)} ${getCurrentUser()}"
+    }
+
 
 }
