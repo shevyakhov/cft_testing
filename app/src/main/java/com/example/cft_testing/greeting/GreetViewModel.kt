bb55d402ef4566295912e17e53ec27aea77ed5da
shevyakhov <shevyakhov89@gmail.com>
2022-09-14 14:11:32 +0700

    greet user function finished

--- app/src/main/java/com/example/cft_testing/greeting/GreetViewModel.kt
+++ app/src/main/java/com/example/cft_testing/greeting/GreetViewModel.kt
@@ -1,16 +1,28 @@
 package com.example.cft_testing.greeting
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
 import com.example.cft_testing.R
 
 class GreetViewModel(val app: Application) : AndroidViewModel(app) {
     private fun getCurrentUser(): String {
-        return "sup"
+        val sharedPrefs: SharedPreferences =
+            app.getSharedPreferences(app.getString(R.string.sharedPrefs), Context.MODE_PRIVATE)
+        val surname = sharedPrefs.getString(
+            app.getString(R.string.sharedPrefsSurname),
+            app.getString(R.string.empty)
+        )
+        val name = sharedPrefs.getString(
+            app.getString(R.string.sharedPrefsName),
+            app.getString(R.string.empty)
+        )
+        return "$surname $name"
     }
 
     fun greetCurrentUser(): String {
-        return "${app.getString(R.string.hello)} ${getCurrentUser()}"
+        return "${app.getString(R.string.hello)} ${getCurrentUser()}!!!"
     }
 
 
